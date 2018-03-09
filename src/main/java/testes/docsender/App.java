d00a50db75147ef07d1fd043d7e1741789607d18
Carlos Adolfo Rebouças Gondim <8880@TTJDEINFU144561.tj.ce.gov.br>
2018-03-09 15:03:31 -0300

    Fazendo a APP funcionar

--- src/main/java/testes/docsender/App.java
+++ src/main/java/testes/docsender/App.java
@@ -22,7 +22,7 @@ import org.json.JSONObject;
 public class App {
 
 	private static String url = "http://localhost:3000/documents/create";
-	private static String fileName = "/home/tj.ce.gov.br/8880/Documentos/wiki_cnc.pdf";
+	private static String fileName = "/home/tj.ce.gov.br/8880/Downloads/feeds(1).opml";
 
 	public static void main(String[] args) {
 		// Boolean success = true;
@@ -37,34 +37,27 @@ public class App {
 			postMethod.setHeader("Content-type", "application/json");
 			postMethod.setHeader("Data-type", "json");
 			httpClient.execute(postMethod, responseHandler);
+			System.out.println(((BasicResponseHandler) responseHandler).toString());
 		} catch (Exception exception) {
 			exception.printStackTrace();
 			// Log.d("Uploader Class Error", "Error code: " + error.getStatusCode());
 			// Log.d("Uploader Class Error", "Error message: " + error.getMessage());
 			// success = false;
-		}
+		}		
 		// Log.d("server resposne", response);
 		// return success;
 	}
 
 	public static JSONObject constructPictureJson() throws JSONException, IOException {
-		//String userId = "1";
-		//String folderId = "1";
 		String[] file = fileName.split("/");
 		JSONObject fileData = new JSONObject();		
-		//pictureData.put("user_id", userId);
-		//pictureData.put("folder_id", folderId);
-		//pictureData.put("picture_name", "picture name");
-		//pictureData.put("picture_description", "1");
-		//pictureData.put("content_type", "pdf");
-		//pictureData.put("original_filename", "base64:" + file[file.length - 1]);
 		fileData.put("file_name", file[file.length - 1]);
-		fileData.put("file", encodePicture(fileName));
+		fileData.put("file", encodeFile(fileName));
 
 		return fileData;
 	}
 
-	public static String encodePicture(String fileName) throws IOException {
+	public static String encodeFile(String fileName) throws IOException {
 		File picture = new File(fileName);		
 		return Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(picture));
 	}
